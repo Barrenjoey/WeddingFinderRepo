89f14a1db4ef85571f563e13dcca6f45943f2e2c
Jake Van der meer <J_vdm@hotmail.com>
2020-05-04 16:00:18 +1000

    Form Validation

--- WeddingFinder/Controllers/ListBusinessController.cs
+++ WeddingFinder/Controllers/ListBusinessController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WeddingFinder.Models;
+using WeddingFinder.ViewModels;
 
 namespace WeddingFinder.Controllers
 {
@@ -12,7 +15,37 @@ namespace WeddingFinder.Controllers
         {
             ViewBag.SearchData = SearchData;
             ViewBag.ImgUrl = "/images/background2.JPG";
-            return View();
+            return View(PopulateDropdowns());
+        }
+
+        [HttpPost]
+        public IActionResult SubmitForm(ListBusinessFormViewModel formModel)
+        {
+            // Server side validation (in new project)
+            // - Category Not Equal default choice.
+            if (formModel.Category == null || formModel.Category == "Please select one")
+            {
+                return View("Index", formModel);                
+            }            
+
+
+            // Email to inbox
+
+
+            // Redirect to Thankyou page
+            return Content("test");
+        }
+
+        private ListBusinessFormViewModel PopulateDropdowns()
+        {
+            ListBusinessFormViewModel formViewModel = new ListBusinessFormViewModel();
+            List<Category> categoryList = new List<Category>();
+            categoryList = Context.Category.ToList();
+            formViewModel.CategoryList = categoryList;
+            //formViewModel.CategoryList = new SelectList(categoryList, "CategoryId", "CategoryName");
+            //formViewModel.CategoryList.Prepend(new SelectListItem { Text = "Please choose one", Value = "" });
+
+            return formViewModel;
         }
     }
 }
---
+++ WeddingFinder/ViewModels/ListBusinessFormViewModel.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WeddingFinder.Models;
+
+namespace WeddingFinder.ViewModels
+{
+    public class ListBusinessFormViewModel
+    {
+        [DisplayName("First Name")]
+        [Required]
+        [StringLength(20)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage ="First Name must contain letters only")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last Name must contain letters only")]
+        public string LastName { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email  { get; set; }
+
+        [DisplayName("Contact Number")]
+        [Required]  
+        [StringLength(10)]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Contact Number must contain numbers only")]
+        public string ContactNumber { get; set; }
+        
+        [Required]
+        [StringLength(70)]
+        [RegularExpression(@"^[a-zA-Z0-9/ ]*$", ErrorMessage = "Street address contains invalid characters")]
+        public string Street { get; set; }
+        
+        [Required]
+        [StringLength(20)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Suburb must contain letters only")]
+        public string Suburb { get; set; }
+
+        [Required]
+        [StringLength(4)]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Postcode must contain numbers only")]
+        public string Postcode { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "State is invalid")]
+        public string State { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Region is invalid")]
+        public string Region { get; set; }
+
+        [DisplayName("Business Name")]
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[a-zA-Z0-9 ]*$", ErrorMessage = "Business Name must contain letters or numbers only")]
+        public string BusinessName { get; set; }
+
+        [Required]        
+        [StringLength(30)]
+        [RegularExpression(@"^[a-zA-Z&]+$", ErrorMessage = "Category is invalid")]
+        public string Category { get; set; }
+
+        [DisplayName("Instagram Url")]        
+        [StringLength(70)]
+        [RegularExpression(@"^.*(instagram.com).*", ErrorMessage = "Url is invalid")]
+        public string Instagram { get; set; }
+
+        [DisplayName("Facebook Url")]        
+        [StringLength(70)]
+        [RegularExpression(@"^.*(facebook.com).*$", ErrorMessage = "Url is invalid")]
+        public string Facebook { get; set; }
+
+        [DisplayName("Website Url")]
+        [StringLength(70)]
+        public string Website { get; set; }
+
+        [DisplayName("Short Description")]
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9/&#!'.,?:;-_ ]*$", ErrorMessage = "Description contains invalid characters")]
+        //[StringLength(50,MinimumLength=20)]
+        public string ShortDescription { get; set; }
+
+        [DisplayName("Full Description")]
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9/&#!'.,?:;-_ ]*$", ErrorMessage = "Description contains invalid characters")]
+        //[StringLength(200, MinimumLength = 50)]
+        public string FullDescription { get; set; }        
+
+        [Required]
+        public List<Category> CategoryList { get; set; }
+    }
+}
